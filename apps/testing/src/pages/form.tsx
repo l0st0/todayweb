01562fe308f2e5e74cfc03facd185865a1c22421
l0st0 <r.bilina@protonmail.com>
2023-02-09 17:31:00 +0100

    add zod to form

--- apps/testing/src/pages/form.tsx
+++ apps/testing/src/pages/form.tsx
@@ -2,20 +2,24 @@ import { LoadingButton } from "@mui/lab";
 import { Form, FormTextInput } from "@todayweb/cms";
 import React from "react";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const scheme = z.object({
+  email: z.string().email("Zadaj platný e-mail."),
+  password: z.string().min(8, "Heslo musí obsahovať minimálne 8 znakov."),
+});
+
+type Form = z.infer<typeof scheme>;
 
 const FormPage = () => {
-  const { control, handleSubmit, reset } = useForm({
-    defaultValues: { oldPassword: "", password: "", confirmPassword: "" },
+  const { control } = useForm({
+    defaultValues: { email: "", password: "" },
   });
 
   return (
     <Form>
-      <FormTextInput name="password" control={control} label="Staré heslo" />
-      <FormTextInput
-        name="confirmPassword"
-        control={control}
-        label="Staré heslo"
-      />
+      <FormTextInput control={control} name="password" label="Staré heslo" />
+      <FormTextInput control={control} name="email" label="Staré heslo" />
 
       <LoadingButton fullWidth>Submit</LoadingButton>
     </Form>
